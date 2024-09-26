473dfbed2d352a421da2050f0471b44e4dd42faa
reuven <r0583247229@gmail.com>
2024-09-26 16:01:45 +0300

    add sql files with queries and indexing

---
+++ sql/indexing.sql
@@ -0,0 +1,25 @@
+--query 1
+
+SELECT air_force, COUNT(*) AS mission_count
+FROM mission
+WHERE EXTRACT(YEAR FROM mission_date) = 1943
+GROUP BY air_force
+ORDER BY mission_count DESC
+LIMIT 1;
+
+--b-tree index
+CREATE INDEX idx_mission_date ON mission (extract (YEAR FROM mission_date))
+--hash index
+
+--query 2
+select bomb_damage_assessment, count(target_country) from mission
+where bomb_damage_assessment is not null
+and airborne_aircraft > 5
+group by target_country, bomb_damage_assessment
+order by count(bomb_damage_assessment) desc limit 1
+
+--b-tree index
+CREATE INDEX idx_bomb_damage_assessment ON mission (bomb_damage_assessment)
+
+
+
